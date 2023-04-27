fc65258072a43b47dfb259578f59b419eb39bba0
oliviasculley <olivia@sculley.dev>
2023-04-26 23:03:04 -0400

    improve event class

--- c4g/event.py
+++ c4g/event.py
@@ -30,59 +30,25 @@ class Event:
     @classmethod
     def from_event_json(cls, event_json):
         """Create an event class object from the raw event json returned by the OpenApi"""
-        location = ""
-        if event_json['venue'] is None:
-            location = None
-        elif event_json['venue']['name'] is not None and event_json['venue']['address'] is not None:
-            name = event_json['venue']['name']
-            address = event_json['venue']['address']
-            city = event_json['venue']['city']
-            state = event_json['venue']['state']
-            zip_code = event_json['venue']['zip']
-            location = f"{name} at {address} {city}, {state} {zip_code}"
-        elif event_json['venue']['lat'] is not None and event_json['venue']['lat']:
-            location = f"lat/long: {event_json['venue']['lat']}, {event_json['venue']['lat']}"
-        elif event_json['venue']['name'] is not None:
-            location = event_json['venue']['name']
-
-        status = ""
-        if event_json['status'] == "upcoming":
-            status = "Upcoming ✅"
-        elif event_json['status'] == "past":
-            status = "Past ✔"
-        elif event_json['status'] == "cancelled":
-            status = "Cancelled ❌"
-        else:
-            status = event_json['status'].title()
-
-        time = parser.isoparse(event_json['time']).astimezone(
-            pytz.timezone(os.environ.get("TZ"))).strftime('%B %-d, %Y %I:%M %p %Z')
-
         return cls(
             title=event_json['event_name'],
             group_name=event_json['group_name'],
             description=event_json['description'],
-            location=location,
-            time=time,
+            location=parse_location(event_json),
+            time=parser.isoparse(event_json['time']),
             url=event_json['url'],
-            status=status,
+            status=event_json['status'],
             uuid=event_json['uuid']
         )
 
     def generate_blocks(self):
-        """Compose a slack message using the blocks layout"""
-        if self.location is None:
-            location = "No location"
-        elif self.location is not None:
-            location = ("<https://www.google.com/maps/search/?api=1&query="
-                        f"{urllib.parse.quote(self.location)}|{self.location}>")
-
+        """Compose part of a slack message using the blocks layout"""
         return [
             {
                 "type": "header",
                 "text":  {
                     "type": "plain_text",
-                    "text": f"{truncate_string(self.title)}"
+                    "text": truncate_string(self.title)
                 }
             },
             {
@@ -90,12 +56,12 @@ class Event:
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": f"By {truncate_string(self.group_name)}"
+                        "text": truncate_string(self.group_name)
 
                     },
                     {
                         "type": "mrkdwn",
-                        "text": f"<{truncate_string(self.url)}|*Link* :link:>"
+                        "text": f"<{self.url}|*Link* :link:>"
                     }
                 ]
             },
@@ -103,11 +69,8 @@ class Event:
                 "type": "section",
                 "text": {
                     "type": "plain_text",
-                    "text": f"{truncate_string(self.description)}"
+                    "text": truncate_string(self.description)
                 }
-            },
-            {
-                "type": "divider"
             },
             {
                 "type": "section",
@@ -119,7 +82,7 @@ class Event:
                     },
                     {
                         "type": "mrkdwn",
-                        "text": f"{truncate_string(self.status)}"
+                        "text": print_status(self.status)
                     }
                 ]
             },
@@ -133,7 +96,7 @@ class Event:
                     },
                     {
                         "type": "mrkdwn",
-                        "text": location
+                        "text": get_location_url(self.location)
                     }
                 ]
             },
@@ -146,7 +109,7 @@ class Event:
                     },
                     {
                         "type": "plain_text",
-                        "text": f"{self.time}"
+                        "text": print_datetime(self.time)
                     }
                 ]
             }
@@ -155,13 +118,60 @@ class Event:
     def generate_text(self):
         """Compose a text string of event information for backup"""
         return (f"Name: {truncate_string(self.title)}\n"
-                f"Link: {truncate_string(self.url)}\n"
+                f"Link: {self.url}\n"
                 f"Description: {truncate_string(self.description)}\n"
-                f"Status: {self.status}\n"
+                f"Status: {print_status(self.status)}\n"
                 f"Location: {self.location}\n"
-                f"Time: {self.time}"
-                )
+                f"Time: {print_datetime(self.time)}")
+
+
+def parse_location(event_json):
+    """Parse location string from event json"""
+    if event_json['venue'] is None:
+        return None
+
+    if None not in (event_json['venue']['name'],
+                    event_json['venue']['address'], event_json['venue']['city'],
+                    event_json['venue']['state'], event_json['venue']['zip']):
+        return (f"{event_json['venue']['name']} at "
+                f"{event_json['venue']['address']} {event_json['venue']['city']}, "
+                f"{event_json['venue']['state']} {event_json['venue']['zip']}")
+
+    if event_json['venue']['lat'] is not None and event_json['venue']['lat']:
+        return f"lat/long: {event_json['venue']['lat']}, {event_json['venue']['lat']}"
+
+    return f"{event_json['venue']['name']}"
+
 
 def truncate_string(string, length=500):
     """Truncate string and add ellipses if it's too long"""
     return string[:length] + (string[length:] and "...")
+
+
+def get_location_url(location):
+    """Return google maps link for location or plaintext"""
+    if location is None:
+        return "No location"
+
+    return ("<https://www.google.com/maps/search/?api=1&query="
+            f"{urllib.parse.quote(location)}|{location}>")
+
+
+def print_status(status):
+    """Prints status with emojis :D"""
+    if status == "upcoming":
+        return "Upcoming ✅"
+
+    if status == "past":
+        return "Past ✔"
+
+    if status == "cancelled":
+        return "Cancelled ❌"
+
+    return status.title()
+
+
+def print_datetime(time):
+    """Print datetime in local timezone as string"""
+    return time.astimezone(pytz.timezone(os.environ.get("TZ"))
+                           ).strftime('%B %-d, %Y %I:%M %p %Z')
