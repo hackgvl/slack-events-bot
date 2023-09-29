6e0d56e8b276fc543be35792d3172074751e1731
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-09-28 23:29:57 -0400

    Add a couple of tests

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi.testclient import TestClient
+from threading import Thread
+
+from bot import API
+
+@pytest.fixture
+def test_client():
+    """Returns a Starlette test API instance"""
+    return TestClient(API)
+
+@pytest.fixture
+def threads_appear_dead(monkeypatch):
+    """Include this fixture if you'd like for all your threads to be reported as dead."""
+    monkeypatch.setattr(Thread, 'is_alive', lambda x : False)
