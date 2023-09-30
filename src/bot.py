ad7fd73f57fa8077d5ab7f2ad20915a9b1256b25
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-09-30 19:14:41 -0400

    Pt 2: Just cache

--- src/bot.py
+++ src/bot.py
@@ -10,6 +10,9 @@ import traceback
 import aiohttp
 import pytz
 import uvicorn
+
+from asyncache import cached
+from cachetools import TTLCache
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import PlainTextResponse
 from slack_bolt.async_app import AsyncApp
@@ -17,6 +20,9 @@ from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
 import database
 
 from event import Event
+
+# 50kb - Current API responses are around ~11.7kb as of 09/30/2023
+MAX_BYTES = 50000
 
 # configure app
 APP = AsyncApp(
@@ -81,6 +87,7 @@ async def trigger_check_api(ack, say, logger, command):
         await check_api(CONN)
 
 
+@cached(cache=TTLCache(maxsize=MAX_BYTES, ttl=15 * 60))
 async def check_api(conn):
     """Check the api for updates and update any existing messages"""
     async with aiohttp.ClientSession() as session:
@@ -197,36 +204,6 @@ async def post_or_update_messages(conn, week, blocks, text):
 
 
 API = FastAPI()
-
-
-def check_api_being_requested(path: str, payload: bytes) -> bool:
-    """Determines if a user is attempting to execute the /check_api command."""
-    decoded_payload = payload.decode("utf-8")
-
-    return path == "/slack/events" and "command=%2Fcheck_api" in decoded_payload
-
-
-def check_api_on_cooldown() -> bool:
-    """Checks to see if the /check_api command has been run in the last 15 minutes."""
-    return True
-
-
-@API.middleware("http")
-async def rate_limit_check_api(req: Request, call_next):
-    """Looks to see if /check_api has been run recently, and returns an error if so."""
-
-    if (
-        check_api_being_requested(req.scope["path"], await req.body())
-        and check_api_on_cooldown()
-    ):
-        return PlainTextResponse(
-            (
-                "This command has been run recently and is on a cooldown period. "
-                "Please try again in a little while!"
-            )
-        )
-
-    return await call_next(req)
 
 
 @API.post("/slack/events")
