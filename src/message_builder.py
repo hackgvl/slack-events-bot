ad2a9ff0601637e40aef031aa80071a405772970
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-10-24 00:14:45 -0400

    Multi-post support

---
+++ src/message_builder.py
@@ -0,0 +1,126 @@
+"""
+Contains logic to spread events over a number of Slack posts evenly to ensure that
+the bot doesn't exceed any of Slack's limitations of the number of blocks (50) or
+the length of text (4000 characters) that a single message can contain.
+"""
+
+import datetime
+import math
+
+from event import Event
+
+# This is lower than the actual limit to provide headroom
+MAX_MESSAGE_CHARACTER_LENGTH = 3000
+
+
+async def build_header(week_start: datetime.datetime, index: int, total: int) -> dict:
+    """
+    Return a header for an image
+    """
+    text = (
+        f"HackGreenville Events for the week of {week_start.strftime('%B %-d')}"
+        f" - {index} of {total}\n\n===\n\n"
+    )
+
+    return {
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": (
+                        "HackGreenville Events for the week of "
+                        f"{week_start.strftime('%B %-d')} - {index} of {total}"
+                    ),
+                },
+            },
+            {"type": "divider"},
+        ],
+        "text": text,
+        "text_length": len(text),
+    }
+
+
+async def build_single_event_block(
+    event_data, week_start: datetime.datetime, week_end: datetime.datetime
+) -> dict | None:
+    """
+    Returns the blocks (content and divider), text, and text length for a single event
+    """
+    event = Event.from_event_json(event_data)
+
+    # ignore event if it's not in the current week
+    if event.time < week_start or event.time > week_end:
+        return
+
+    # ignore event if it has a non-supported status
+    if event.status not in ["cancelled", "upcoming", "past"]:
+        print(f"Couldn't parse event {event.uuid} " f"with status: {event.status}")
+        return
+
+    text = f"{event.generate_text()}\n\n"
+
+    return {
+        "blocks": event.generate_blocks() + [{"type": "divider"}],
+        "text": text,
+        "text_length": len(text),
+    }
+
+
+async def build_event_blocks(resp, week_start, week_end) -> list:
+    """
+    Build out all of the blocks and text for all events
+
+    Strips out any blanks before returning
+    """
+    return list(
+        filter(
+            bool,
+            [
+                await build_single_event_block(event, week_start, week_end)
+                for event in await resp.json()
+            ],
+        )
+    )
+
+
+async def chunk_messages(event_blocks, week_start) -> list:
+    """
+    Chunk up events across messages so that no one message is longer than 4k characters
+    """
+    total_messages_needed = math.ceil(
+        sum(event["text_length"] for event in event_blocks)
+        / MAX_MESSAGE_CHARACTER_LENGTH
+    )
+
+    messages = []
+
+    initial_header = await build_header(week_start, 1, total_messages_needed)
+
+    blocks = initial_header["blocks"]
+    text = initial_header["text"]
+
+    for event in event_blocks:
+        # Event can be safely added to existing message
+        if event["text_length"] + len(text) < MAX_MESSAGE_CHARACTER_LENGTH:
+            blocks += event["blocks"]
+            text += event["text"]
+            continue
+
+        # Save message and then start a new one
+        messages += [{"blocks": blocks, "text": text}]
+
+        new_header = await build_header(
+            week_start, len(messages) + 1, total_messages_needed
+        )
+
+        blocks = new_header["blocks"]
+        text = new_header["text"]
+
+        blocks += event["blocks"]
+        text += event["text"]
+
+    # Add whatever is left as a new message
+    messages += [{"blocks": blocks, "text": text}]
+
+    return messages
---
+++ tests/test_bot.py
@@ -0,0 +1,42 @@
+"""
+Tests the core Slack integration functionality of the application.
+"""
+
+import datetime
+import pytest
+import pytz
+
+from bot import post_or_update_messages
+import database
+
+TEST_SLACK_CHANNEL_ID = "fake_slack_id"
+
+week = datetime.datetime.strptime("10/22/2023", "%m/%d/%Y").replace(tzinfo=pytz.utc)
+
+
+@pytest.mark.asyncio
+async def test_post_or_update_messages_too_many_new_events(caplog, db_cleanup):
+    """
+    post_or_update_messages fails if it determines that too many
+    new events have been added since a previous post was sent to a
+    Slack channel. "Too many" means that an increase in total messages
+    would occur, which can have unpredictable results.
+    """
+
+    # Create a "channel" and a message
+    await database.add_channel(TEST_SLACK_CHANNEL_ID)
+    await database.create_message(
+        "2023-10-22 00:00:00+00:00",
+        "test",
+        "1698119853.135399",
+        TEST_SLACK_CHANNEL_ID,
+        1,
+    )
+
+    await post_or_update_messages(week, ["message 1", "message 2"])
+
+    assert (
+        "Updating messages would cause us to hit the Slack rate limit. "
+        "This is because new events have been added. "
+        "Existing message count: 1 --- New message count: 2." in caplog.text
+    )
